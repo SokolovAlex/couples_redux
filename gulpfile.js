012ae20a59a3075ac3993316696df6581293218c
sokolov alex <alex.sklv85@gmail.com>
2016-03-21 02:31:13 +0600

    start react

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require("gulp"),
+    browserify = require("browserify"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    source = require('vinyl-source-stream'),
+    webserver = require('gulp-webserver'),
+    babelify = require('babelify');
+
+var watch_src = ['src/js/*.js'];
+
+gulp.task('clean', () => {
+    return rimraf("src/build/**/*.*", { nosort: true, silent: true }, function(){});
+});
+
+gulp.task('js', () => {
+    return browserify('src/js/app.js')
+        .transform(babelify, {
+            presets: [require('babel-preset-stage-2'), require('babel-preset-es2015'), 'react'],
+            plugins: [require('babel-plugin-transform-runtime')]
+        })
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('src/build'));
+});
+
+gulp.task("watch", () => {
+    gulp.watch(watch_src, ['js', 'css']);
+});
+
+gulp.task("css", () => {
+    return gulp.src('src/css/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('src/build/'))
+});
+
+gulp.task('webserver', () =>{
+    return gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            port: 2222,
+            directoryListing: true,
+            open: true
+        }));
+});
+
+gulp.task("build", ['clean', 'js', 'css', 'watch']);
+
+gulp.task("default", ['build', 'webserver']);
