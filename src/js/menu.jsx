08d22d6688cd177dd6774b112a5ebb710ef5cf73
Alex Sokolov <alex.sklv85@gmail.com>
2016-03-28 13:21:49 +0600

    menu link component

--- src/js/menu.jsx
+++ src/js/menu.jsx
@@ -4,31 +4,52 @@
 import React, { Component } from 'react';
 import { selectMenu } from './actions';
 
-let store;
+export default class MenuLink extends Component {
+    render() {
+        let name = this.props.name;
+        this.store = this.props.store;
+        this.screenName = this.props.screenName;
+        let active = this.props.currentPage === name;
+        let onMenuClick = this.onClick.bind(this, name);
+        return (
+            <li
+                onClick={onMenuClick}
+                className={active ? 'active' : null}>
+                <a href="#">{this.screenName}</a>
+            </li>
+        );
+    }
+    onClick(menu) {
+        let store = this.store;
+        store.dispatch(selectMenu(menu));
+    }
+}
 
 export default class Menu extends Component {
     render() {
-        store = this.props.store;
+        let store = this.props.store;
         let page = store.getState().menu;
         return (
             <ul className="nav masthead-nav">
-                <li
-                    onClick={this.onClick}
-                    name="home"
-                    className={page === 'home' ? 'active' : null}><a href="#">Home</a></li>
-                <li
-                    onClick={this.onClick}
-                    name="couples"
-                    className={page === 'couples' ? 'active' : null}><a href="#">Couples</a></li>
-                <li
-                    onClick={this.onClick}
-                    name="about"
-                    className={page === 'about' ? 'active' : null}><a href="#">About</a></li>
+                <MenuLink
+                    name = "home"
+                    screenName = "Home"
+                    store = {store}
+                    currentPage = {page}
+                />
+                <MenuLink
+                    name = "couples"
+                    screenName = "Couples"
+                    store = {store}
+                    currentPage = {page}
+                />
+                <MenuLink
+                    name = "about"
+                    screenName = "About"
+                    store = {store}
+                    currentPage = {page}
+                />
             </ul>
         );
-    }
-    onClick(e) {
-        var name = e.currentTarget.getAttribute("name");
-        store.dispatch(selectMenu(name));
     }
 }
